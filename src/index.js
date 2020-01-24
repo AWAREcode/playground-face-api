595d94191b0d0be26367b2b5e5b4181cf5e50938
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-24 14:29:43 +0100

    Load models from face-api.js repo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD");
+async function loadModels() {
+    const MODEL_URL =
+        "https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js@0.22.0/weights/";
+
+    await faceapi.loadSsdMobilenetv1Model(MODEL_URL);
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    await faceapi.loadFaceRecognitionModel(MODEL_URL);
+}
+
+function main() {
+    loadModels();
+}
+
+main();
