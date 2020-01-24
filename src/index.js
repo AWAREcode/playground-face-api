e70f642f167ca00f6c42635fd10a6c505b6559c0
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-24 14:54:56 +0100

    Add example image, add style.css, detect face from image

--- src/index.js
+++ src/index.js
@@ -7,8 +7,43 @@ async function loadModels() {
     await faceapi.loadFaceRecognitionModel(MODEL_URL);
 }
 
-function main() {
-    loadModels();
+async function detectFaceFromImg(imgElement) {
+    showLoading();
+
+    const fullFaceDescriptions = await faceapi
+        .detectAllFaces(imgElement)
+        .withFaceLandmarks()
+        .withFaceDescriptors();
+
+    hideLoading();
+
+    console.log(fullFaceDescriptions);
+    // output(fullFaceDescriptions);
+}
+
+function showLoading() {
+    const loaderEl = getLoaderElement();
+    loaderEl.classList.remove("hidden");
+}
+
+function hideLoading() {
+    const loaderEl = getLoaderElement();
+    loaderEl.classList.add("hidden");
+}
+
+function getLoaderElement() {
+    const el = document.getElementsByClassName("loader-wrapper")[0];
+    if (!el)
+        throw new Error("No loader element");
+    return el;
+}
+
+async function main() {
+    await loadModels();
+
+    const imgEl = document.getElementById("image");
+    if (imgEl)
+        detectFaceFromImg(imgEl);
 }
 
 main();
